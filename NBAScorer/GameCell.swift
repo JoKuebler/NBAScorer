7f296a4fe6827eff95c3c1110293d989e4b734b9
JoKuebler <jonikuebler@gmail.com>
2019-03-02 18:32:28 +0100

    Refactored GameCell and deleted unused code

--- NBAScorer/GameCell.swift
+++ NBAScorer/GameCell.swift
@@ -12,11 +12,20 @@ import UIKit
 
 class GameCell: UITableViewCell {
     
-    var awayTeam = ""
-    var homeTeam = ""
-    
+    // Objects needed
+    let contentCell = UIView()
+    let iconAway = UIImageView()
+    let iconHome = UIImageView()
+    let scoreExcitement = UILabel()
+    let teamNameAway = UILabel()
+    let teamNameHome = UILabel()
+    let arenaName = UILabel()
+
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setupUIView()
+        setupUIImageView()
+        setupLabels()
         setupCellView()
     }
     
@@ -24,98 +33,31 @@ class GameCell: UITableViewCell {
         fatalError("init(coder:) has not been implemented")
     }
     
-    let contentCell: UIView = {
-        let cellView = UIView()
-        cellView.backgroundColor = UIColor(red: 254/255, green: 255/255, blue: 254/255, alpha: 1)
-        cellView.layer.cornerRadius = 15
-        cellView.layer.borderWidth = 1
-        cellView.layer.borderColor = UIColor(red: 231/255, green: 233/255, blue: 237/255, alpha: 1).cgColor
-        return cellView
-        
-    }()
+    // Sets up styles of Cell
+    func setupUIView() {
+        contentCell.backgroundColor = UIColor(red: 254/255, green: 255/255, blue: 254/255, alpha: 1)
+        contentCell.layer.cornerRadius = 15
+        contentCell.layer.borderWidth = 1
+        contentCell.layer.borderColor = UIColor(red: 231/255, green: 233/255, blue: 237/255, alpha: 1).cgColor
+    }
     
-    // ImageView for away icon
-    let iconAway: UIImageView = {
-        let imageViewAway = UIImageView()
-        //imageViewAway.image = UIImage(named: "hawks.png")!
-        imageViewAway.backgroundColor = .green
-        return imageViewAway
-    }()
+    // Sets up styles of ImageViews
+    func setupUIImageView() {
+        iconAway.backgroundColor = .green
+        iconHome.backgroundColor = .black
+    }
     
-    // ImageView for home icon
-    let iconHome: UIImageView = {
-        let imageViewHome = UIImageView()
-        //imageViewHome.image = UIImage(named: "hawks.png")!
-        imageViewHome.backgroundColor = .black
-        return imageViewHome
-    }()
+    // Sets up styles of Labels
+    func setupLabels() {
+        scoreExcitement.font = UIFont(name: "OpenSans-Bold", size: 25)
+        scoreExcitement.text = "10.0"
+        scoreExcitement.textAlignment = .center
+        teamNameAway.font = UIFont(name: "OpenSans-Bold", size: 20)
+        teamNameHome.font = UIFont(name: "OpenSans-Bold", size: 20)
+        arenaName.font = UIFont(name: "OpenSans-Bold", size: 15)
+    }
     
-    // Excitement Score Label
-    let scoreExcitement: UILabel = {
-        let exScoreLabel = UILabel()
-        exScoreLabel.text = "9.8"
-        exScoreLabel.font = exScoreLabel.font.withSize(30)
-        exScoreLabel.font = UIFont.boldSystemFont(ofSize: exScoreLabel.font.pointSize)
-        exScoreLabel.font = UIFont(name: "OpenSans-Bold", size: 25)
-        //exScoreLabel.backgroundColor = .blue
-        exScoreLabel.textAlignment = .center
-        return exScoreLabel
-    }()
-    
-    let teamNameAway: UILabel = {
-        let awayTeamLabel = UILabel()
-        awayTeamLabel.font = awayTeamLabel.font.withSize(30)
-        awayTeamLabel.font = UIFont.boldSystemFont(ofSize: awayTeamLabel.font.pointSize)
-        awayTeamLabel.font = UIFont(name: "OpenSans-Bold", size: 20)
-        return awayTeamLabel
-    }()
-    
-    let teamNameHome: UILabel = {
-        let homeTeamLabel = UILabel()
-        homeTeamLabel.font = homeTeamLabel.font.withSize(30)
-        homeTeamLabel.font = UIFont.boldSystemFont(ofSize: homeTeamLabel.font.pointSize)
-        homeTeamLabel.font = UIFont(name: "OpenSans-Bold", size: 20)
-        return homeTeamLabel
-    }()
-    
-    let arenaName: UILabel = {
-        let arenaLabel = UILabel()
-        arenaLabel.font = arenaLabel.font.withSize(30)
-        arenaLabel.font = UIFont.boldSystemFont(ofSize: arenaLabel.font.pointSize)
-        arenaLabel.font = UIFont(name: "OpenSans-Bold", size: 15)
-        return arenaLabel
-    }()
-    
-    //    let scoreAway: UILabel = {
-    //        let awayScoreLabel = UILabel()
-    //        awayScoreLabel.text = "125"
-    //        awayScoreLabel.font = awayScoreLabel.font.withSize(25)
-    //        awayScoreLabel.font = UIFont.boldSystemFont(ofSize: awayScoreLabel.font.pointSize)
-    //        awayScoreLabel.backgroundColor = .blue
-    //        awayScoreLabel.textAlignment = .center
-    //        return awayScoreLabel
-    //    }()
-    //
-    //    let scoreHome: UILabel = {
-    //        let homeScoreLabel = UILabel()
-    //        homeScoreLabel.text = "97"
-    //        homeScoreLabel.font = homeScoreLabel.font.withSize(25)
-    //        homeScoreLabel.font = UIFont.boldSystemFont(ofSize: homeScoreLabel.font.pointSize)
-    //        homeScoreLabel.backgroundColor = .blue
-    //        homeScoreLabel.textAlignment = .center
-    //        return homeScoreLabel
-    //    }()
-    //
-    //    let separator: UILabel = {
-    //        let separatorLabel = UILabel()
-    //        separatorLabel.backgroundColor = .red
-    //        separatorLabel.text = "-"
-    //        separatorLabel.font = separatorLabel.font.withSize(25)
-    //        separatorLabel.font = UIFont.boldSystemFont(ofSize: separatorLabel.font.pointSize)
-    //        separatorLabel.textAlignment = .center
-    //        return separatorLabel
-    //    }()
-    
+    // Sets up cell View and add constraints to all elements
     func setupCellView() {
         
         // Add subview to cell contentView
@@ -126,10 +68,6 @@ class GameCell: UITableViewCell {
         contentCell.addSubview(teamNameHome)
         contentCell.addSubview(arenaName)
         self.contentView.addSubview(contentCell)
-        //        self.contentView.addSubview(scoreAway)
-        //        self.contentView.addSubview(scoreHome)
-        //        self.contentView.addSubview(separator)
-        
         
         // Enables constraints for labels
         iconAway.translatesAutoresizingMaskIntoConstraints = false
@@ -139,12 +77,8 @@ class GameCell: UITableViewCell {
         teamNameHome.translatesAutoresizingMaskIntoConstraints = false
         arenaName.translatesAutoresizingMaskIntoConstraints = false
         contentCell.translatesAutoresizingMaskIntoConstraints = false
-        //        scoreAway.translatesAutoresizingMaskIntoConstraints = false
-        //        scoreHome.translatesAutoresizingMaskIntoConstraints = false
-        //        separator.translatesAutoresizingMaskIntoConstraints = false
         
-     
-        
+        // Activate constraints
         NSLayoutConstraint.activate([
             
             // Creats content with margin to cell borders
@@ -177,21 +111,6 @@ class GameCell: UITableViewCell {
             // Arena Constraints
             arenaName.topAnchor.constraint(equalTo: contentCell.topAnchor, constant: 10),
             arenaName.centerXAnchor.constraint(equalTo: contentCell.centerXAnchor)
-            
-            
-
-            //            scoreAway.widthAnchor.constraint(equalToConstant: 64),
-            //            scoreAway.heightAnchor.constraint(equalToConstant: 35),
-            //            scoreAway.leadingAnchor.constraint(equalTo: iconAway.trailingAnchor, constant: 25),
-            //            scoreAway.centerYAnchor.constraint(equalTo: self.centerYAnchor),
-            //            scoreHome.widthAnchor.constraint(equalToConstant: 64),
-            //            scoreHome.heightAnchor.constraint(equalToConstant: 35),
-            //            scoreHome.trailingAnchor.constraint(equalTo: iconHome.leadingAnchor, constant: -25),
-            //            scoreHome.centerYAnchor.constraint(equalTo: self.centerYAnchor),
-            //            separator.widthAnchor.constraint(equalToConstant: 10),
-            //            separator.heightAnchor.constraint(equalToConstant: 10),
-            //            separator.leadingAnchor.constraint(equalTo: scoreAway.trailingAnchor, constant: 5),
-            //            separator.centerYAnchor.constraint(equalTo: self.centerYAnchor)
 
             ])
 
